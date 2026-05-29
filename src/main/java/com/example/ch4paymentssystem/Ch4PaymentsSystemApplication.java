041c4521efcb386239f392067735cf73f38428d0
0younge <s45628439@gmail.com>
2026-05-29 14:23:17 +0900

    first commit

---
+++ src/main/java/com/example/ch4paymentssystem/Ch4PaymentsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch4paymentssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4PaymentsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4PaymentsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch4paymentssystem/Ch4PaymentsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch4paymentssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4PaymentsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
