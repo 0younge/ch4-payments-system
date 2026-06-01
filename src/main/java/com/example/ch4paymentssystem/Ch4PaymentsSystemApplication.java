1367b36e8d34a28105525ed0149bf7c1782a9a11
0younge <s45628439@gmail.com>
2026-06-01 15:07:56 +0900

    feat: baseEntity 개발

--- src/main/java/com/example/ch4paymentssystem/Ch4PaymentsSystemApplication.java
+++ src/main/java/com/example/ch4paymentssystem/Ch4PaymentsSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.ch4paymentssystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Ch4PaymentsSystemApplication {
 
     public static void main(String[] args) {
