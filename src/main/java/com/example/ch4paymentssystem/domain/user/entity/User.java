f788941cc30a64398c7eff92e3f464c25d2f239c
0younge <s45628439@gmail.com>
2026-06-01 15:06:25 +0900

    feat: user 기본 틀 개발

---
+++ src/main/java/com/example/ch4paymentssystem/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.ch4paymentssystem.domain.user.entity;
+
+import com.example.ch4paymentssystem.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer pointBalance = 0;
+
+}
