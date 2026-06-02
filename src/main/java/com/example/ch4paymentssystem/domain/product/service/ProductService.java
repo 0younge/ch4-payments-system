599554eabdd102822db1ff2416c27f9813bed52d
minbyeongjun <byeongjunmin00@gmail.com>
2026-06-02 10:46:47 +0900

    feat: 상품 단건 조회 API 구현

--- src/main/java/com/example/ch4paymentssystem/domain/product/service/ProductService.java
+++ src/main/java/com/example/ch4paymentssystem/domain/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.ch4paymentssystem.domain.product.service;
 
+import com.example.ch4paymentssystem.domain.product.dto.ProductResponseDto;
+import com.example.ch4paymentssystem.domain.product.entity.Product;
 import com.example.ch4paymentssystem.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,12 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    public ProductResponseDto getProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+        return new ProductResponseDto(product);
+    }
 
 }
